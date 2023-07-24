05aa691e1011af0d272ace35bf6f205997cb4e10
nfcdjobo <nfcdjobo@gmail.com>
2023-07-24 18:04:33 +0000

    add api url

--- src/api_url/api_url.js
+++ src/api_url/api_url.js
@@ -1,5 +1,6 @@
 
-// Dans un fichier isolé on met le lien de base de l'api puis on l'exporte
-const api_url = 'http://localhost:3001/';
+// Dans un fichier isolé on met le lien de base de l'api puis on l'exporte   
+// const api_url = 'http://localhost:3001/';
+const api_url = 'https://art-exhibition-management-platform-api.onrender.com/';
 export default api_url;
 
